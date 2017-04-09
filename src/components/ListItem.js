65c6719a212c0e1f7a578826a231bec5630098aa
mjdude <mo@syncspot.net>
2017-04-09 12:04:32 -0700

    added action creator to a component

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import {Text} from 'react-native';
+import {connect} from 'react-redux';
 import {CardSection} from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
     render(){
         const {titleStyle} = styles;
+        console.log(this.props)
         return (
             <CardSection>
                 <Text style={titleStyle}>{this.props.library.title}</Text>
@@ -20,4 +23,9 @@ const styles = {
     }
 }
 
-export default ListItem;
+// doing it this way allows us to pass in and dispatch actions
+// without needing to call dispatch from props
+// null is in place of returning the state
+// no need to call dispatch ourselves
+
+export default connect(null, actions)(ListItem);
