4646d7e6189facc7888d37280f4fb30e4d3c2e62
mjdude <mo@syncspot.net>
2017-04-09 12:26:30 -0700

    renderdescription for tech stack

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,6 +5,17 @@ import {CardSection} from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+
+    renderDescription(){
+        const {library, selectedLibraryId} = this.props;
+
+        if (library.id === selectedLibraryId) {
+            return (
+                <Text>{library.description}</Text>
+            )
+        }
+    }
+
     render(){
         const {titleStyle} = styles;
         const {id, title} = this.props.library;
@@ -17,6 +28,7 @@ class ListItem extends Component {
                     <CardSection>
                         <Text style={titleStyle}>{title}</Text>
                     </CardSection>
+                    {this.renderDescription()}
                 </View>
             </TouchableWithoutFeedback>
           
@@ -36,4 +48,8 @@ const styles = {
 // null is in place of returning the state
 // no need to call dispatch ourselves
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state) => {
+    return { selectedLibraryId: state.selectedLibraryId}
+}
+
+export default connect(mapStateToProps, actions)(ListItem);
